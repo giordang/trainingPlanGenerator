09c83b273e5c8a50722ec403cea7b3075bb295c2
Gregory Giordano <greggior@Gregorys-MacBook-Pro.local>
2019-06-11 22:15:36 -0600

    server update

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ var app = express();
 var path = require('path');
 
 app.use(express.static(path.join(__dirname)));
-app.use("/CSS", express.static(__dirname));
-app.use("/Images", express.static(__dirname));
-app.use("/JS", express.static(__dirname));
+app.use("/styles", express.static(__dirname + '/CSS'));
+app.use("/images", express.static(__dirname + '/Images'));
+app.use("/scripts", express.static(__dirname + '/JS'));
 
 // viewed at based directory http://localhost:8080/
 app.get('/', function (req, res) {
